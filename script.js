804e7d16cd2a97ac2d8928d341f59be1dcc14f2f
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-05-11 22:32:50 +0200

    feat: add styling to done tasks and write code to only accept more than 5 characters for input

--- script.js
+++ script.js
@@ -6,16 +6,12 @@ function updateLocalStorage() {
 
 function syncToDos() {
   const toDosFromLocalStorage = localStorage.getItem("toDos");
-
   if (toDosFromLocalStorage) {
     toDos = JSON.parse(toDosFromLocalStorage);
   }
 }
 
 syncToDos();
-
-//initial sync
-//addToLocalStorage();
 
 function renderToDoApp() {
   const toDoList = document.querySelector("#todo-list");
@@ -40,6 +36,10 @@ function renderSingleToDo(toDo) {
 
   const toDoLi = document.createElement("li");
 
+  if (toDo.done === true) {
+    toDoLi.style.textDecoration = "line-through";
+  }
+
   const toDoCheckbox = document.createElement("input");
   toDoCheckbox.type = "checkbox";
   toDoCheckbox.checked = toDo.done;
@@ -47,7 +47,6 @@ function renderSingleToDo(toDo) {
   toDoCheckbox.addEventListener("change", () => {
     toDo.done = !toDo.done;
     updateLocalStorage();
-    renderToDoApp();
   });
 
   toDoLi.appendChild(toDoCheckbox);
@@ -55,8 +54,9 @@ function renderSingleToDo(toDo) {
   toDoList.appendChild(toDoLi);
 
   const editInput = document.createElement("input");
+
   editInput.value = toDo.description;
-  editInput.style.width = "100%";
+  editInput.style.width = "min-content";
   editInput.readOnly = true;
 
   const editBtn = document.createElement("button");
@@ -72,35 +72,57 @@ function renderSingleToDo(toDo) {
 
       toDo.description = editInput.value;
 
-      updateLocalStorage();
-      renderToDoApp();
+      if (editInput.value.length < 5) {
+        alert("Please insert more than 5 characters.");
+        editInput.readOnly = false;
+        editBtn.textContent = "Save";
+      } else {
+        updateLocalStorage();
+        renderToDoApp();
+      }
     }
   });
 
-  toDoList.appendChild(editInput);
+  toDoLi.appendChild(editInput);
 
   toDoList.appendChild(editBtn);
-  /*
-  const toDoTextNode = document.createTextNode(toDo.description);
-  toDoLi.appendChild(toDoTextNode);*/
+
+  if (toDoCheckbox.checked === true) {
+    toDoCheckbox.nextElementSibling.style.textDecoration = "line-through";
+  } else {
+    toDoCheckbox.nextElementSibling.style.textDecoration = "none";
+  }
 }
 
-const addToDoButton = document.querySelector("#add-todo");
-addToDoButton.addEventListener("click", () => {
+const addToDoWithButton = document.querySelector("#add-todo");
+addToDoWithButton.addEventListener("click", addToDo);
+
+const addToDoWithEnter = document.querySelector("#new-todo");
+addToDoWithEnter.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addToDo();
+  }
+});
+
+function addToDo() {
   const newToDoInput = document.querySelector("#new-todo");
   const newToDo = {
     description: newToDoInput.value,
     done: false,
   };
+  if (newToDoInput.value.length < 5) {
+    alert("Please insert at least 5 characters.");
+  } else {
+    toDos.push(newToDo);
+    updateLocalStorage();
 
-  toDos.push(newToDo);
-  updateLocalStorage();
-
-  newToDoInput.value = "";
-  renderToDoApp();
-});
+    newToDoInput.value = "";
+    renderToDoApp();
+  }
+}
 
 renderToDoApp();
+updateLocalStorage();
 
 const toDoFilters = document.querySelector("#filter");
 toDoFilters.addEventListener("change", (e) => {
@@ -111,13 +133,26 @@ function getCurrentFilter() {
   return document.querySelector('input[name="filter"]:checked').value;
 }
 
+renderToDoApp();
+
 const btnDeleteAll = document.querySelector("#btn-delete-all");
 btnDeleteAll.addEventListener("click", (e) => {
   toDos = toDos.filter((toDo) => !toDo.done);
   updateLocalStorage();
-
   renderToDoApp();
 });
+
+function changeDoneStyle(e) {
+  if (e.target.checked === true) {
+    e.target.nextElementSibling.style.textDecoration = "line-through";
+  } else {
+    e.target.nextElementSibling.style.textDecoration = "none";
+  }
+}
+
+const toDoList = document.querySelector("#todo-list");
+toDoList.addEventListener("change", changeDoneStyle);
+
 //Another option
 /*
 function renderToDos() {
